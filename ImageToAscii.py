945b445061185d8d1a59a5d6ed63f803971d2948
Just1 <62154678+123justin123@users.noreply.github.com>
2022-01-27 14:28:56 +0100

    Add files via upload

--- ImageToAscii.py
+++ ImageToAscii.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
-import math, cv2
+import math
+import cv2
+import argparse
 
-chars = ["B","S","#","&","@","$","%","*","!",":","."][::-1]
+chars = ["B", "S", "#", "&", "@", "$", "%", "*", "!", ":", "."][::-1]
+
+
 def resize(img):
     width, height = img.size
     ratio = height / width / 1.65
@@ -19,11 +23,13 @@ def image_to_ascii(path):
     pixels = img.getdata()
     characters = "".join([chars[pix*len(chars)//256] for pix in pixels])
     lg = len(characters)
-    ascii_image = "\n".join([characters[i:i+width] for i in range(0, lg, width)])
+    ascii_image = "\n".join([characters[i:i+width]
+                            for i in range(0, lg, width)])
     with open("result.txt", "w") as f:
         f.write(ascii_image)
     return ascii_image
-            
+
+
 def frame_to_ascii(img):
     img = resize(img)
     width, height = img.size
@@ -31,14 +37,14 @@ def frame_to_ascii(img):
     pixels = grey_img.getdata()
     characters = "".join([chars[pix * len(chars) // 256] for pix in pixels])
     lg = len(characters)
-    ascii_image = "\n".join([characters[i:i+width] for i in range(0, lg, width)])
+    ascii_image = "\n".join([characters[i:i+width]
+                            for i in range(0, lg, width)])
     print(ascii_image)
-    
 
-   
-def video_ascii(video = 0):   
+
+def video_ascii(video=0):
     cap = cv2.VideoCapture(video)
-    while True:  
+    while True:
         ret, frame = cap.read()
         if ret:
             frame_to_ascii(Image.fromarray(frame))
@@ -46,7 +52,16 @@ def video_ascii(video = 0):
         else:
             break
 
+def test():
+    print("test")
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help="Chemin de l'image")
+parser.add_argument('-c', action='store_true', help="Convertir l'image de la caméra")
+args = parser.parse_args()
 
-test = [[1,2,3],[4,5,6]]
-print(test[1])
+if(args.input):
+    image_to_ascii(args.input)
+
+if(args.c):
+    video_ascii()
